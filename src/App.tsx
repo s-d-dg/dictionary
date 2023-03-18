43d444161cea8d6ae2b66cb5eeb14061ccbcab4e
Damian Gąsiorowski <softwaredevelopment.dg@gmail.com>
2023-03-18 09:35:57 +0100

    [dictionary] - global styles adjusted

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import styles from './App.module.css';
+import Content from './components/content/content';
 
 function App() {
   return (
     <div className={styles.layout}>
       <h1>Dictionary</h1>
+      <Content />
     </div>
   );
 }
