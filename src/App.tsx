799d9468cf87a162c7d1faf129e3ef95f9506851
Damian Gąsiorowski <softwaredevelopment.dg@gmail.com>
2023-03-18 08:46:58 +0100

    [dictionary] - content centering

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div >
+    <div className={styles.layout}>
       <h1>Dictionary</h1>
     </div>
   );
