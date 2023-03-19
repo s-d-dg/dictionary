23d7e0657de0dbf79d3f8a91d77d375b96a8acf5
Damian Gąsiorowski <softwaredevelopment.dg@gmail.com>
2023-03-19 13:13:02 +0100

    [dictionary] - no results found text added

--- src/components/content/dictionary/wordList/wordList.tsx
+++ src/components/content/dictionary/wordList/wordList.tsx
@@ -6,6 +6,7 @@ interface WordListPropTypes {
 
 const WordList = ({ words }: WordListPropTypes) => {
     return <div className={styles['words-container']}>
+        {words.length === 0 && <div>No results found</div>}
         {words.map((word: string) => (<div className={styles['single-word']} key={word}>{word}</div>))}
     </div>
 }
